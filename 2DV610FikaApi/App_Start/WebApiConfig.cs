ca86767fdc373477c6f51137c8c96e7996585942
SheriefBadran <sb222rf@student.lnu.se>
2015-11-10 15:00:58 +0100

    RED: testing api controller Get(), all currently existing api controller actions are auto generated.

--- 2DV610FikaApi/App_Start/WebApiConfig.cs
+++ 2DV610FikaApi/App_Start/WebApiConfig.cs
@@ -17,7 +17,7 @@ namespace _2DV610FikaApi
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { id = RouteParameter.Optional }      
             );
         }
     }
