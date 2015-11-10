aafc40a4178b24ad17f877f4798a910211e363d4
Grenmyr <dg222cs@student.lnu.se>
2015-11-10 13:56:39 +0100

    RED: created first test to ensure baker name can not be null on initialization

---
+++ 2DV610FikaApi.Tests/Models/BakerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2DV610FikaApi.Models;
+
+namespace _2DV610FikaApi.Tests
+{
+    [TestClass]
+    public class BakerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowExceptionWhenBakerNameIsNull()
+        {
+            string name = null;
+            Baker baker = new Baker(name);
+        }
+    }
+}
---
+++ 2DV610FikaApi/Models/Baker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2DV610FikaApi.Models
+{
+    public class Baker
+    {
+        private string name;
+        public Baker(string name)
+        {
+            this.name = name;
+        }
+    }
+}
