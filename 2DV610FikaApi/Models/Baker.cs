a3eac6ef7ebdd7b3d5a816456df6addf1e5a432f
Grenmyr <dg222cs@student.lnu.se>
2015-11-10 15:16:31 +0100

    GREEN: test now pass, added Constructor that take 2 arguments leaving the old one intact

--- 2DV610FikaApi/Models/Baker.cs
+++ 2DV610FikaApi/Models/Baker.cs
@@ -8,6 +8,7 @@ namespace _2DV610FikaApi.Models
     public class Baker
     {
         private string name;
+        private string email;
         public Baker(string name)
         {
             if (String.IsNullOrEmpty(name))
@@ -15,6 +16,15 @@ namespace _2DV610FikaApi.Models
                 throw new ArgumentException();
             }
             this.name = name;
+        }
+
+        public Baker(string name, string email)
+        {
+            if (email.Length < 4)
+            {
+                throw new ArgumentException();
+            }
+            this.email = email;
         }
     }
 }
