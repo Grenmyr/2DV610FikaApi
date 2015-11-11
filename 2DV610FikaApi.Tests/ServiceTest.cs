e831ccb01bc349f723db5c6651f0e9cf65e621df
Grenmyr <dg222cs@student.lnu.se>
2015-11-11 14:23:37 +0100

    RED: Setting up first test for a service expect repository to get one call

---
+++ 2DV610FikaApi.Tests/ServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2DV610FikaApi.Models.Repositories;
+
+namespace _2DV610FikaApi.Tests
+{
+    [TestClass]
+    public class ServiceTest
+    {
+        [TestMethod]
+        public void ShouldCallFikaRepositoryOnce()
+        {
+            Mock<IFikaRepository> mock = new Mock<IFikaRepository>();
+            Service service = new Service(mock.Object);
+
+            service.GetFikas();
+
+            mock.Verify(fr => fr.GetFikas(), Times.Once);
+        }
+    }
+}
---
+++ 2DV610FikaApi/Service.cs
@@ -0,0 +1,21 @@
+﻿using _2DV610FikaApi.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2DV610FikaApi
+{
+    public class Service
+    {
+
+        public Service(IFikaRepository repository)
+        {
+            throw new NotImplementedException();
+        }
+        public void GetFikas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
