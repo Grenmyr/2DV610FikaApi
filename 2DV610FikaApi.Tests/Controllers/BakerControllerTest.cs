0697eb7687e40fa6c15596bb33e045ac8f7ef167
SheriefBadran <sb222rf@student.lnu.se>
2015-11-11 11:19:10 +0100

    RED: should be able to get a list of bakers.

---
+++ 2DV610FikaApi.Tests/Controllers/BakerControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using _2DV610FikaApi.Controllers;
+
+namespace _2DV610FikaApi.Tests.Controllers
+{
+    [TestClass]
+    public class BakerControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new BakerController();
+            var allBakers = controller.Get();
+
+            Assert.AreEqual(0, allBakers);
+            // Expect empty list if no bakers in the system.
+        }
+    }
+}
