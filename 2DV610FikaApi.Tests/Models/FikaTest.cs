d822cfabe2901baf71f603617e2e531a7c7cf05e
Grenmyr <dg222cs@student.lnu.se>
2015-11-13 10:46:36 +0100

    RED: Test expecting a porperty with datetime and expect same datetime object

---
+++ 2DV610FikaApi.Tests/Models/FikaTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2DV610FikaApi.Models;
+
+namespace _2DV610FikaApi.Tests.Models
+{
+    [TestClass]
+    public class FikaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DateTime date = new DateTime()
+            Fika fika = new Fika(date);
+
+            Assert.AreEqual(date, fika.Date);
+        }
+    }
+}
