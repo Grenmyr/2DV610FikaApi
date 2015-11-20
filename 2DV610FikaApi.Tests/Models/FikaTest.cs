7c12997d0bcbcec1833f7d8b9c83a4fe8b7cda89
Grenmyr <dg222cs@student.lnu.se>
2015-11-20 14:28:02 +0100

    RED: David and Sherief added two tests that verify Fika property Pastry cannot be set to null or emty strings

--- 2DV610FikaApi.Tests/Models/FikaTest.cs
+++ 2DV610FikaApi.Tests/Models/FikaTest.cs
@@ -33,5 +33,23 @@ namespace _2DV610FikaApi.Tests.Models
 
             Assert.AreEqual(_validPastry, fika.Pastry);
         }
+
+        [TestMethod]
+        public void FikaPastryPropertyDoesNotAllowNullValues()
+        {
+            Fika fika = new Fika(_validDateTime, null);
+
+            Assert.IsNotNull(fika.Pastry);
+
+        }
+
+        [TestMethod]
+        public void FikaPastryPropertyDoesNotAllowEmtryStringValues()
+        {
+            Fika fika = new Fika(_validDateTime, String.Empty);
+
+            Assert.AreNotEqual(String.Empty, fika.Pastry);
+
+        }
     }
 }
