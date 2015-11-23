adc91359d5c84fc06405e1dae25f714ca107199b
SheriefBadran <sb222rf@student.lnu.se>
2015-11-23 16:00:44 +0100

    REFACTOR: Created a TestInitialize for the service test.

--- 2DV610FikaApi.Tests/ServiceTest.cs
+++ 2DV610FikaApi.Tests/ServiceTest.cs
@@ -10,40 +10,47 @@ namespace _2DV610FikaApi.Tests
     [TestClass]
     public class ServiceTest
     {
+
+         Mock<IFikaRepository> _fikaMock;
+         Mock<IBakerRepository> _bakerMock;
+         Service _fikaService;
+         Service _bakerService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _fikaMock = new Mock<IFikaRepository>();
+            _bakerMock = new Mock<IBakerRepository>();
+            _fikaService = new Service(_fikaMock.Object);
+            _bakerService = new Service(_bakerMock.Object);
+        }
+
         [TestMethod]
         public void ServiceGetFikasShouldInvokeRepositoryGetFikasOnce()
         {
-            Mock<IFikaRepository> mock = new Mock<IFikaRepository>();
-            Service service = new Service(mock.Object);
+            _fikaService.GetFikas();
 
-            service.GetFikas();
-
-            mock.Verify(fr => fr.GetFikas(), Times.Once);
+            _fikaMock.Verify(fr => fr.GetFikas(), Times.Once);
         }
 
         [TestMethod]
         public void ServiceGetFikasReturnsExpectedFikaList()
         {
             List<Fika> emptyList = new List<Fika>();
-            Mock<IFikaRepository> mock = new Mock<IFikaRepository>();
-            Service service = new Service(mock.Object);
-            mock
+            _fikaMock
                 .Setup(repository => repository.GetFikas())
                 .Returns(emptyList);
 
-            List<Fika> result = service.GetFikas();
+            List<Fika> result = _fikaService.GetFikas();
             Assert.AreSame(emptyList, result);
         }
 
         [TestMethod]
         public void ServiceGetBakersShouldInvokeRepositoryGetBakersOnce()
         {
-            Mock<IBakerRepository> mock = new Mock<IBakerRepository>();
-            Service service = new Service(mock.Object);
+            _bakerService.GetBakers();
 
-            service.GetBakers();
-
-            mock.Verify(s => s.GetBakers(), Times.Once);
+            _bakerMock.Verify(s => s.GetBakers(), Times.Once);
         }
 
         [TestMethod]
@@ -57,15 +64,13 @@ namespace _2DV610FikaApi.Tests
                 It.IsAny<Baker>()
             };
             List<Baker> expectedEmptyList = new List<Baker>();
-            Mock<IBakerRepository> mock = new Mock<IBakerRepository>();
-            Service service = new Service(mock.Object);
-            mock
-                .SetupSequence(repository => repository.GetBakers())
-                .Returns(expectedPopulatedList)
-                .Returns(expectedEmptyList);
+            _bakerMock
+                 .SetupSequence(repository => repository.GetBakers())
+                 .Returns(expectedPopulatedList)
+                 .Returns(expectedEmptyList);
 
-            List<Baker> populatedResult = service.GetBakers();
-            List<Baker> emptyResult = service.GetBakers();
+            List<Baker> populatedResult = _bakerService.GetBakers();
+            List<Baker> emptyResult = _bakerService.GetBakers();
 
             Assert.AreEqual(expectedPopulatedList.Count, populatedResult.Count);
             Assert.AreEqual(expectedEmptyList.Count, emptyResult.Count);
@@ -76,13 +81,11 @@ namespace _2DV610FikaApi.Tests
         {
             List<Baker> nullValue = (List<Baker>)null;
             List<Baker> expectedList = new List<Baker>();
-            Mock<IBakerRepository> mock = new Mock<IBakerRepository>();
-            Service service = new Service(mock.Object);
-            mock
+            _bakerMock
                 .Setup(repository => repository.GetBakers())
                 .Returns(nullValue);
 
-            List<Baker> result = service.GetBakers();
+            List<Baker> result = _bakerService.GetBakers();
 
             Assert.IsNotNull(result);
             Assert.AreEqual(expectedList.Count, result.Count);
