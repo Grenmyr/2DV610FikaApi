985de00f1a686ff5385e853d1fef3e0b8bfc1ae8
Grenmyr <dg222cs@student.lnu.se>
2015-11-24 18:20:42 +0100

    GREEN: All tests now pass, next i will rename tests as they do no longer expect exception as mentioned in testnames

--- 2DV610FikaApi/Models/Baker.cs
+++ 2DV610FikaApi/Models/Baker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 
@@ -8,25 +9,25 @@ namespace _2DV610FikaApi.Models
 {
     public class Baker
     {
-        private string _name;
-        private string _email;
+     
 
         public Baker(string name, string email)
         {
-            //if (name == null
-            //    || !Extensions.IsWithin(name.Length, 1, 20)
-            //    || !Extensions.IsWithin(email.Length, 4, 254))
-            //{
-            //    throw new ArgumentException();
-            //}
-            //_name = name;
-            //_email = email;
+            Name = name;
+            Email = email;
         }
 
-        public string Email
-        {
-            get { return _email; }
-        }
+    
+        [Required(ErrorMessage="Email can not be null.")]
+        [MinLength(4,ErrorMessage="Email need to be atleast 4 characters long.")]
+        [MaxLength(254,ErrorMessage="Email can not be longer then 254 characters.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Name can not be null.")]
+        [MinLength(1, ErrorMessage = "Name need to be atleast 1 characters long.")]
+        [MaxLength(20, ErrorMessage = "Name can not be longer then 20 characters.")]
+        public string Name { get; set; }
+
 
         public int Id { get; set; }
 
