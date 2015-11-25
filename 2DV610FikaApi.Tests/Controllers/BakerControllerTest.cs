532ab83d0e1310b080a55155304f02f2654f3ca0
SheriefBadran <sb222rf@student.lnu.se>
2015-11-25 23:21:33 +0100

    RED: One test to verify that controller action put invokes service method InvokeBaker once. Another test to assert that OkNegotiatedContentResult is returned with updated baker on existing id.

--- 2DV610FikaApi.Tests/Controllers/BakerControllerTest.cs
+++ 2DV610FikaApi.Tests/Controllers/BakerControllerTest.cs
@@ -134,6 +134,40 @@ namespace _2DV610FikaApi.Tests.Controllers
         }
 
         [TestMethod]
+        public void BakerServicePutBakerShouldBeInvokedOnceWhenBakerControllerPutActionIsCalled()
+        {
+            Baker baker = new Baker("David", "david.grenmyr@mail.com");
+            _service.Setup(service => service.GetBaker(It.IsAny<int>())).Returns(baker);
+            _controller.Put(baker);
+
+            _service
+                .Verify(service => service.PutBaker(It.IsAny<int>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void BakerControllerPutMethodShouldReturnABakerAsContentForExistingIdAndStatusCodeOk()
+        {
+            int existingId = 1;
+            Baker existingBaker = new Baker("David", "david.grenmyr@mail.com");
+            existingBaker.Id = existingId;
+            Baker updatedBaker = new Baker("David Grenmyr", "david.grenmyr@mail.com");
+            updatedBaker.Id = existingId;
+            _service
+                .Setup(service => service.GetBaker(existingId))
+                .Returns(existingBaker);
+            _service
+                .Setup(service => service.PutBaker(existingId))
+                .Returns(updatedBaker);
+
+            var baker = _controller.Put(existingBaker) as OkNegotiatedContentResult<Baker>;
+
+            // Assert that status code is 200 Ok
+            Assert.IsNotNull(baker);
+            Assert.IsNotNull(baker.Content);
+            Assert.IsInstanceOfType(baker.Content, typeof(Baker));
+        }
+
+        [TestMethod]
         public void BakerServiceDeleteBakerShouldBeInvokedOnceWhenBakerControllerDeleteActionIsCalled()
         {
             _controller.Delete(It.IsAny<int>());
